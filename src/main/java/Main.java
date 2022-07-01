96b3286b3dfd6d10585dd0bd26fc875254579c21
Guzzler13 <stephanedasil@gmail.com>
2022-07-01 15:39:00 +0200

    clean code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,22 +11,16 @@ public class Main {
         int diag1 = 0;
         int diag2 = 0;
 
-        System.out.println("longueur matrice " + matrice.length);
         for (int i = 0; i < matrice.length; i++) {
             for (int j = 0; j < matrice[i].length; j++) {
-                System.out.print(i + " ");
-                System.out.print(j + " ");
 
                 //On cherche la somme de la diagonale 1
                 if (i == j) {
-                    System.out.println(" /// i == j ///");
                     diag1 += matrice[i][j];
                 }
 
                 //On cherche la somme de la diagonale 2
                 if (i + j == matrice.length - 1) {
-                    System.out.println(" /// i + j == matrice.length - 1 ///");
-
                     diag2 += matrice[i][j];
                 }
             }
