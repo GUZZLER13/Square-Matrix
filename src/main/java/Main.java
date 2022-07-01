5bf4d61b06c9ba38922b3c9790ae5873c4f4c517
Guzzler13 <stephanedasil@gmail.com>
2022-07-01 11:40:15 +0200

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[][] matrice = new int[][]{
+                {1, 2, 3, 4, 5},
+                {2, 3, 4, 5, 7},
+                {3, 4, 5, 6, 12},
+                {4, 5, 6, 7, 13},
+                {5, 6, 7, 8, 14}
+        };
+
+        int diag1 = 0;
+        int diag2 = 0;
+
+        //On cherche la somme de la diagonale 1
+        System.out.println("grandeur matrice " + matrice.length);
+        for (int i = 0; i < matrice.length; i++) {
+            for (int j = 0; j < matrice[i].length; j++) {
+                System.out.print(i + " ");
+                System.out.print(j + " ");
+                if (i == j) {
+                    System.out.println(" /// i == j ///");
+                    diag1 += matrice[i][j];
+                }
+            }
+        }
+        System.out.println("diag1 = " + diag1);
+
+        //On cherche la somme de la diagonale 2
+        for (int i = 0; i < matrice.length; i++) {
+            for (int j = 0; j < matrice[i].length; j++) {
+                System.out.print(i + " ");
+                System.out.print(j + " ");
+                if (i + j == matrice.length - 1) {
+                    System.out.println(" /// i + j == matrice.length - 1 ///");
+
+                    diag2 += matrice[i][j];
+                }
+            }
+        }
+        System.out.println("diag2 = " + diag2);
+        System.out.println("difference des diag = " + (diag1 - diag2));
+    }
+}
