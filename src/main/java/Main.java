bd343a2a0fe8717e2c9d8c042dea257370106496
Guzzler13 <stephanedasil@gmail.com>
2022-07-01 15:59:37 +0200

    ajout fonction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,30 @@
 public class Main {
     public static void main(String[] args) {
 
-
-        int[][] matrice = new int[][]{
+        int[][] matrice1 = new int[][]{
                 {1, 2, 3, 4, 5},
                 {2, 3, 4, 5, 7},
                 {3, 4, 5, 6, 12},
                 {4, 5, 6, 7, 13},
                 {5, 6, 7, 8, 14}
         };
+        System.out.println(sumDiag(matrice1));
+    }
 
-        int diag1 = 0;
-        int diag2 = 0;
+    public static int sumDiag(int[][] matrice) {
+        int sumDiag1 = 0;
+        int sumDiag2 = 0;
 
         for (int i = 0; i < matrice.length; i++) {
             for (int j = 0; j < matrice[i].length; j++) {
-
-                //On cherche la somme de la diagonale 1
                 if (i == j) {
-                    diag1 += matrice[i][j];
+                    sumDiag1 += matrice[i][j];
                 }
-
-                //On cherche la somme de la diagonale 2
                 if (i + j == matrice.length - 1) {
-                    diag2 += matrice[i][j];
+                    sumDiag2 += matrice[i][j];
                 }
             }
         }
-        System.out.println("diag1 = " + diag1);
-        System.out.println("diag2 = " + diag2);
-        System.out.println("difference des diag = " + (diag1 - diag2));
+        return sumDiag1 - sumDiag2;
     }
 }
