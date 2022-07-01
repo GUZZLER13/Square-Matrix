447bd46ffd65835fb842a05433d9685cd5fcf0e1
Guzzler13 <stephanedasil@gmail.com>
2022-07-01 15:09:32 +0200

    une seule recherche dans les deux tableaux

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
         int[][] matrice = new int[][]{
                 {1, 2, 3, 4, 5},
                 {2, 3, 4, 5, 7},
@@ -12,25 +11,19 @@ public class Main {
         int diag1 = 0;
         int diag2 = 0;
 
-        //On cherche la somme de la diagonale 1
-        System.out.println("grandeur matrice " + matrice.length);
+        System.out.println("longueur matrice " + matrice.length);
         for (int i = 0; i < matrice.length; i++) {
             for (int j = 0; j < matrice[i].length; j++) {
                 System.out.print(i + " ");
                 System.out.print(j + " ");
+
+                //On cherche la somme de la diagonale 1
                 if (i == j) {
                     System.out.println(" /// i == j ///");
                     diag1 += matrice[i][j];
                 }
-            }
-        }
-        System.out.println("diag1 = " + diag1);
 
-        //On cherche la somme de la diagonale 2
-        for (int i = 0; i < matrice.length; i++) {
-            for (int j = 0; j < matrice[i].length; j++) {
-                System.out.print(i + " ");
-                System.out.print(j + " ");
+                //On cherche la somme de la diagonale 2
                 if (i + j == matrice.length - 1) {
                     System.out.println(" /// i + j == matrice.length - 1 ///");
 
@@ -38,6 +31,7 @@ public class Main {
                 }
             }
         }
+        System.out.println("diag1 = " + diag1);
         System.out.println("diag2 = " + diag2);
         System.out.println("difference des diag = " + (diag1 - diag2));
     }
